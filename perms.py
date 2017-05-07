58f6c76212d892698297d15d25891c0a60bd7d23
Johnny Nguyen <johnny.nguyen39@stclairconnect.ca>
2017-05-07 13:48:00 -0400

    #10: Added a permissions python file to check. Its based on many util functions.

---
+++ perms.py
@@ -0,0 +1,10 @@
+# permissions to check for commands
+# We'll attempt to load from the file again as well.
+from discord.ext import commands
+import discord.utils
+
+# Checks mod permissions
+# This is the only one we really need.
+# TODO: Fix this
+def check_permissions():
+    pass
