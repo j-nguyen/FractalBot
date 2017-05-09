bf3af2460575728a1b61ae49d543d0e9d7e166fb
Johnny Nguyen <jnguyen1236@gmail.com>
2017-05-09 04:52:55 -0400

    #3: Added the kick command

--- ext/mod.py
+++ ext/mod.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import discord
 import logging
+import perms
 
 # Enables us to get a specific log for each extension
 log = logging.getLogger(__name__)
@@ -11,6 +12,24 @@ class Mod:
     def __init__(self, bot):
         self.bot = bot
 
+    # Command to kick the user.
+    # Paramaters:
+    # - discord.Member class Object.
+    # Returns:
+    # - Sends a discord message, returns or not.
+    @commands.command(no_pm=True)
+    async def kick(self, *, member : discord.Member):
+        if 
+        try:
+            await self.bot.kick(member)
+        except discord.Forbidden:
+            await self.bot.say('The bot does not have permissions to kick this member.')
+        except discord.HTTPException:
+            await self.bot.say('Attempt to kick failed.')
+        else:
+            await self.bot.say('Unknown error')
+
+    
 # Helps us add to the extension
 def setup(bot):
     bot.add_cog(Mod(bot))
