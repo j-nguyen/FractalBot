13deb016be13b6b21b2af3fa8d7fa4b2ea419a6e
Johnny Nguyen <johnny.nguyen39@stclairconnect.ca>
2017-05-10 19:32:47 -0400

    #5: Added member join and left events, and finally fixed up event.

--- bot.py
+++ bot.py
@@ -52,12 +52,31 @@ async def on_message_edit(before, after):
 
     # send message as embed
     e = discord.Embed(colour=discord.Colour.dark_gold())
-    e.set_thumbnail(url=usr.avatar_url)
+    e.set_thumbnail(url=usr.avatar_url or usr.default_avatar_url)
     e.timestamp = usr.created_at
     e.set_footer(text='Edited')
     e.set_author(name=str(usr))
     e.add_field(name='Before', value=before.content)
     e.add_field(name='After', value=after.content)
+    channel = bot.get_channel(config['mod_log'])
+
+    # send message
+    try:
+        await bot.send_message(channel, embed=e)
+    except Exception as e:
+        print ('Error: ' + str(e))
+
+@bot.event
+async def on_message_delete(message):
+    usr = message.author
+
+    # send message as embed
+    e = discord.Embed(colour=discord.Colour.dark_gold())
+    e.set_thumbnail(url=usr.avatar_url or usr.default_avatar_url)
+    e.timestamp = datetime.datetime.utcnow()
+    e.set_footer(text='Deleted')
+    e.set_author(name=str(usr))
+    e.add_field(name='Deleted Message', value=message.content)
     channel = bot.get_channel(config['mod_log'])
 
     # send message
@@ -71,6 +90,19 @@ async def on_member_join(member):
     e.timestamp = member.joined_at
     e.set_author(name=str(member))
     e.set_footer(text='Member Joined')
+
+    channel = bot.get_channel(config['mod_log'])
+
+    await bot.send_message(channel, embed=e)
+
+@bot.event
+async def on_member_remove(member):
+    # Create an embed and attempt to join
+    e = discord.Embed(title='Member Left', colour=discord.Colour.green())
+    e.set_thumbnail(url=member.avatar_url or member.default_avatar_url)
+    e.timestamp = datetime.datetime.utcnow()
+    e.set_author(name=str(member))
+    e.set_footer(text='Member Left')
 
     channel = bot.get_channel(config['mod_log'])
 
