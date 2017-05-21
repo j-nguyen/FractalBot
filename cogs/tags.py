a5403d9a5be49091cbf15e95f542af7729882aa7
Johnny Nguyen <jnguyen1236@gmail.com>
2017-05-20 21:09:05 -0400

    #12: Finished up tags feature, ability to add, remove and display, as well as show each one.

--- cogs/tags.py
+++ cogs/tags.py
@@ -23,7 +23,7 @@ class Tags:
     @perms.mod_or_permissions(kick_members=True)
     async def tag(self, ctx):
         if ctx.invoked_subcommand is None:
-            await self.bot.say('Invalid tag command passed')
+            await self.bot.say('Invalid tag command. $tag <add/remove/list/show>')
 
     @tag.command()
     @perms.mod_or_permissions(kick_members=True)
@@ -38,10 +38,50 @@ class Tags:
             sess.commit()
             await self.bot.say('Added tag command.')
         except Exception as e:
-            await self.bot.say(e)
+            await self.bot.say('Cannot add. Reasons: Duplicate entry, or Invalid response.')
         finally:
             sess.close()
 
+    @tag.command()
+    @perms.mod_or_permissions(kick_members=True)
+    async def remove(self, name: str):
+        # Removes the tag
+
+        sess = self.Session()
+
+        tag = sess.query(models.Tag).filter(models.Tag.name == name).first()
+
+        if tag != None:
+            sess.delete(tag)
+            sess.commit()
+            await self.bot.say('Tag *{}* deleted.'.format(name))
+        else:
+            await self.bot.say('Cant find the specified tag!')
+
+    @tag.command()
+    async def list(self):
+        # Shows the list of tags
+        sess = self.Session()
+
+        tags = sess.query(models.Tag).all()
+
+        if tags is None:
+            await self.bot.say('No tags.')
+        else:
+            tagsName = [tag.name for tag in tags]
+            await self.bot.say('Tags: ' + ', '.join(tagsName))
+
+    @tag.command()
+    async def show(self, name: str):
+        # display the tag
+        sess = self.Session()
+
+        tag = sess.query(models.Tag).filter(models.Tag.name == name).first()
+
+        if tag is None:
+            await self.bot.say('Tag cannot be found.')
+        else:
+            await self.bot.say('{}'.format(tag.description))
 
 # Helps us add to the extension
 def setup(bot):
