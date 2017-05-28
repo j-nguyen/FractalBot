7d5063581dc0a3866c2cf4ebecacc4cc672bf9b7
Johnny Nguyen <jnguyen1236@gmail.com>
2017-05-28 16:07:48 -0400

    Changed setup config

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ import json
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import math
 from cogs.utils import models
 
 
@@ -32,23 +31,18 @@ def main():
     # Try doing an initial insert for levels (up to 50)
     ranks = []
     for i in range(50):
-        xp = 100 * math.floor(pow(i, 1.1))
+        xp = 100 * int(round(pow(i, 1.2)))
         rank = models.Rank(xp=xp)
         ranks.append(rank)
 
     # Try addding the ranks
+    db = Session()
     if db.query(models.Rank).count() == 0:
         try:
-            db = Session()
             db.add_all(ranks)
             db.commit()
         except Exception as e:
             print (e)
-
-    # Now add the roles
-    if db.query(models.Role).count() == 0:
-        pass
-
 
 
 if __name__ == '__main__':
