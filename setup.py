997894bb5520fdadca48a038cae301a4462e123c
Johnny Nguyen <jnguyen1236@gmail.com>
2017-05-28 16:02:23 -0400

    Added intial data for rank

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ import json
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import math
 from cogs.utils import models
 
 
@@ -25,6 +26,30 @@ def main():
     models.Role.__table__.create(engine, checkfirst=True)
     models.Level.__table__.create(engine, checkfirst=True)
     models.Topic.__table__.create(engine, checkfirst=True)
+
+    Session = sessionmaker(bind=engine)
+
+    # Try doing an initial insert for levels (up to 50)
+    ranks = []
+    for i in range(50):
+        xp = 100 * math.floor(pow(i, 1.1))
+        rank = models.Rank(xp=xp)
+        ranks.append(rank)
+
+    # Try addding the ranks
+    if db.query(models.Rank).count() == 0:
+        try:
+            db = Session()
+            db.add_all(ranks)
+            db.commit()
+        except Exception as e:
+            print (e)
+
+    # Now add the roles
+    if db.query(models.Role).count() == 0:
+        pass
+
+
 
 if __name__ == '__main__':
     main()
